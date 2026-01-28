0257b2c293eba7f27ef1aa134c02e13784325cdd
zmq <zmq0615@qq.com>
2026-01-28 17:04:06 +0800

    Quartz sync: Jan 28, 2026, 5:04 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -69,7 +69,12 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({
+        markdownLinkResolution: "shortest",
+        openLinksInNewTab: true,
+        lazyLoad: true,
+        externalLinkIcon: true,
+      }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
